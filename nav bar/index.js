2eae5cc46d5c75c9d61011c6a2e68f4dc290e987
Shubhradeep Bose <75801099+Shubhradeep007@users.noreply.github.com>
2021-05-15 12:54:25 -0700

    Add files via upload

---
+++ nav bar/index.js
@@ -0,0 +1,5 @@
+const toogleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+toogleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+}) 
